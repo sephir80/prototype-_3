53e6266c1c4f19a7190d0fd318a2aeccc136157e
Alessandro Tognon <sephir80@gmail.com>
2022-04-27 23:15:09 +0200

    [feat] repeat background continuosly

---
+++ Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float offsetPos = 56;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < startPos.x - offsetPos)
+        {
+            transform.position = startPos;
+        }
+    }
+}
